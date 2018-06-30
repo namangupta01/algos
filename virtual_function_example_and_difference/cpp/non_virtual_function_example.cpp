ed59ba6d3af04be920a7ff296791e60fe791920b
Naman Gupta <01namangupta@gmail.com>
2018-06-30 13:16:20 +0530

    Added Non Virtual function example in CPP

---
+++ virtual_function_example_and_difference/cpp/non_virtual_function_example.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+
+class Base{
+	
+	public:
+		int a;
+		// non virtual function
+		void print(){
+			cout << "In Base::print() function \n";
+		}
+};
+
+class Child : public Base{
+	public:
+		void print(){
+			cout << "In Child::print() function \n";
+		}
+};
+
+int main(){
+	Base b;		
+	b.print();	// Case 1
+
+	Child c;	
+	c.print();	// Case 2
+
+	Base b1 = c;
+	b1.a =10;
+	b1.print();		// Case 3
+
+	Base *b2 = &c;	
+	b2->print();	// Case 4
+	b2->a = 1000;
+	((Base)c).print();	// Case 5
+
+	cout << sizeof(b);
+	cout << sizeof(c);
+	cout << sizeof(b2)<<endl;
+	cout << b1.a<< endl;
+	cout << b2->a<< endl;
+
+	return 0;
+}
+
+
